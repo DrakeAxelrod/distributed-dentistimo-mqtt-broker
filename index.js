22c51a79765a29a68d8064be0c1770aa500f1dbe
Drake Axelrod <drakeaxelrod@gmail.com>
2021-11-24 14:45:43 +0100

    Add authentication to broker

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
 const aedes = require("aedes")();
 const httpServer = require("http").createServer();
 const ws = require("websocket-stream");
-const port = process.env.PORT || 80;
 
 ws.createServer({ server: httpServer }, aedes.handle);
 
+const port = process.env.PORT || 80;
+const user = process.env.BROKER_USERNAME || "admin";
+const pass = process.env.BROKER_PASSWORD || "admin";
+
+const authenticate = (client, username, password, callback) => {
+  if (username === user && password.toString() === pass) {
+    callback(null, true);
+  } else {
+    const error = new Error("Authentication error");
+    error.returnCode = 5;
+    callback(error, false);
+  }
+};
+
 httpServer.listen(port, () => {
   console.log("websocket server listening on port ", port);
+  aedes.authenticate = authenticate
 });
-
-// const Broker = require("./src/broker/index")
-// Broker.start()
